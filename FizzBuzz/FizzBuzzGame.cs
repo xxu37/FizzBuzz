5874fcd530969ad651510af210299f5792445298
Xinxin Xu <xxu37@slb.com>
2023-11-01 14:29:49 +0800

    feat: should return fizz when countOff given a multiple of 3

--- FizzBuzz/FizzBuzzGame.cs
+++ FizzBuzz/FizzBuzzGame.cs
@@ -6,6 +6,11 @@ namespace FizzBuzz
     {
         public static string CountOff(int givenNumber)
         {
+            if (givenNumber % 3 == 0)
+            {
+                return "Fizz";
+            }
+
             return givenNumber.ToString();
         }
     }
