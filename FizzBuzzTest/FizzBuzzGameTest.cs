fec6443e61ec49778f49a2c177780053f37eb6c6
Xinxin Xu <xxu37@slb.com>
2023-11-01 14:25:04 +0800

    test: add test for multiple of 3

--- FizzBuzzTest/FizzBuzzGameTest.cs
+++ FizzBuzzTest/FizzBuzzGameTest.cs
@@ -15,5 +15,16 @@ namespace FizzBuzzTest
             // Then
             Assert.Equal("1", result);
         }
+
+        [Fact]
+        public void Should_return_fizz_when_countOff_given_a_multiple_of_3()
+        {
+            // Given
+            int givenNumber = 3;
+            // When
+            string result = FizzBuzzGame.CountOff(givenNumber);
+            // Then
+            Assert.Equal("Fizz", result);
+        }
     }
 }
